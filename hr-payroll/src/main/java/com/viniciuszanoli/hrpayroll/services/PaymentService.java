bd8c8c03da2ed338c4da81fa275626deace877ca
viniciuszanoli <viniciusdasilva.92@hotmail.com>
2021-04-22 23:31:12 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/viniciuszanoli/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/viniciuszanoli/hrpayroll/services/PaymentService.java
@@ -1,12 +1,28 @@
 package com.viniciuszanoli.hrpayroll.services;
 
 import com.viniciuszanoli.hrpayroll.entities.Payment;
+import com.viniciuszanoli.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String wokerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days){
-        return new Payment("Vinicius", 500.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", ""+workerId);
+        Worker worker = restTemplate.getForObject(wokerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
