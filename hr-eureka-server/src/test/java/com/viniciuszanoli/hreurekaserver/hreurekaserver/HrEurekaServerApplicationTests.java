9b26479995fae542106a16547ef1bc12b0de0dd9
viniciuszanoli <viniciusdasilva.92@hotmail.com>
2021-04-26 21:16:00 -0300

    Configure Eureka Server

---
+++ hr-eureka-server/src/test/java/com/viniciuszanoli/hreurekaserver/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciuszanoli.hreurekaserver.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
