06ab53faf67836f557b87c751dce307cf02cb9ad
viniciuszanoli <viniciusdasilva.92@hotmail.com>
2021-05-12 20:29:49 -0300

    User, Role, many to many association

---
+++ hr-user/src/main/java/com/viniciuszanoli/hruser/hruser/entities/Role.java
@@ -0,0 +1,38 @@
+package com.viniciuszanoli.hruser.hruser.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name="tb_role")
+public class Role implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String roleName;
+
+    public Role() {
+    }
+
+    public Role(Long id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return roleName.equals(role.roleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roleName);
+    }
+}
